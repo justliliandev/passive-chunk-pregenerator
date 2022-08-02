0e911195a185d52269c95071c8c4e19fc60f9fec
agnor99 <kreader100@gmail.com>
2022-08-02 20:01:10 +0200

    mixins are fun

--- Forge/src/main/java/dev/agnor/passivepregen/PassivePregenForge.java
+++ Forge/src/main/java/dev/agnor/passivepregen/PassivePregenForge.java
@@ -4,14 +4,19 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Constants.MOD_ID)
 public class PassivePregenForge {
     
     public PassivePregenForge() {
+
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.addListener(this::onServerShutDown);
         MinecraftForge.EVENT_BUS.addListener(this::onServerStart);
         MinecraftForge.EVENT_BUS.addListener(this::onServerTick);
