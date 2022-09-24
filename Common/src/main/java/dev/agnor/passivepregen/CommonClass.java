ac2d6de494dc2a888bb066bf5d2183ae25be24ac
luftkeks <keksluft@gmail.com>
2022-09-24 17:37:15 +0200

    Port to 1.19.2

--- Common/src/main/java/dev/agnor/passivepregen/CommonClass.java
+++ Common/src/main/java/dev/agnor/passivepregen/CommonClass.java
@@ -4,19 +4,12 @@ import dev.agnor.passivepregen.levelpos.DynamicPlayerLevelPos;
 import dev.agnor.passivepregen.levelpos.ILevelPos;
 import dev.agnor.passivepregen.levelpos.StaticIdentifiableLevelPos;
 import dev.agnor.passivepregen.levelpos.StaticLevelPos;
-import dev.agnor.passivepregen.platform.Services;
 import lombok.Getter;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtIo;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -70,21 +63,21 @@ public class CommonClass {
 
     public static void onServerStart(MinecraftServer server) {
         spawnPoint = new StaticLevelPos(Level.OVERWORLD, server.getWorldData().overworldData().getXSpawn(), server.getWorldData().overworldData().getZSpawn());
-        if (server instanceof DedicatedServer dedicatedServer) {
-            File playerDir = Services.PLATFORM.playerDirectoryFromPlayerList(dedicatedServer.getPlayerList());
-            if (playerDir.isDirectory()) {
-                for (File playerFile : playerDir.listFiles(file -> file.isFile() && file.getName().endsWith(".dat"))) {
-                    try {
-                        CompoundTag read = NbtIo.read(playerFile);
-                        System.out.println(read);
-                    } catch (IOException exception) {
-                        Constants.LOG.warn("playerfile couldn't be read", exception);
-                    }
-                }
-            } else {
-                Constants.LOG.warn("playerDirectory is not a directory");
-            }
-        }
+        //if (server instanceof DedicatedServer dedicatedServer) {
+        //    File playerDir = Services.PLATFORM.playerDirectoryFromPlayerList(dedicatedServer.getPlayerList());
+        //    if (playerDir.isDirectory()) {
+        //        for (File playerFile : playerDir.listFiles(file -> file.isFile() && file.getName().endsWith(".dat"))) {
+        //            try {
+        //                CompoundTag read = NbtIo.read(playerFile);
+        //                System.out.println(read);
+        //            } catch (IOException exception) {
+        //                Constants.LOG.warn("playerfile couldn't be read", exception);
+        //            }
+        //        }
+        //    } else {
+        //        Constants.LOG.warn("playerDirectory is not a directory");
+        //    }
+        //}
     }
 
     public static void onServerStop() {
